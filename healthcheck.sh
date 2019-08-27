e543dc3376064269d552cce9d618812fc55e5e09
j4ym0 <jay_morrall@hotmail.com>
2019-08-27 23:43:26 +0100

    added pin and net stats

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-out="$(ping -W 3 -c 1 -q -s 8 1.1.1.1)"
-[ $? != 0 ] || exit 0
+printf "Checking Connection\n"
+out="$(ping -W 3 -c 4 -s 8 209.222.18.222)"
 printf "$out"
+printf "\n"
+printf "Checking DNS\n"
+out="$(ping -W 3 -c 4 -s 8 google.co.uk)"
+printf "$out"
+printf "\n"
+
+printf "VPN Status\n"
+vpn="$(netstat -i)"
+printf "$vpn \n\n"
 exit 1
