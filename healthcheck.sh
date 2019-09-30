1d7f81df20a9de60cbd0a7056a09913e24fa708c
j4ym0 <jay_morrall@hotmail.com>
2019-09-30 16:48:55 +0100

    working health check

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
 printf "Checking Connection\n"
-out="$(ping -W 3 -c 4 -s 8 209.222.18.222)"
-printf "$out"
-printf "\n"
-printf "Checking DNS\n"
-out="$(ping -W 3 -c 4 -s 8 google.co.uk)"
-printf "$out"
-printf "\n"
+ping -q -c5 privateinternetaccess.com > /dev/null
 
-printf "VPN Status\n"
-vpn="$(netstat -i)"
-printf "$vpn \n\n"
+if [ $? -eq 0 ]
+then
+	echo "ok"
+fi
+
+#failed
 exit 1
