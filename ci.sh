0af06323042409f41ae69f781ba644ceba9c38ba
Quentin McGaw <quentin.mcgaw@gmail.com>
2019-11-23 18:01:18 -0500

    Building Docker images for all CPU architectures (#57)
    * Created Travis config to build images for all CPU architectures
    * Updated readme

---
+++ ci.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
+  docker buildx build --platform=$PLATFORMS  .
+  return $?
+fi
+echo $DOCKER_PASSWORD | docker login -u qmcgaw --password-stdin &> /dev/null
+IMAGE_PATH=$1
+TAG="$TRAVIS_BRANCH"
+if [ "$TAG" = "master" ]; then
+  TAG="${TRAVIS_TAG:-latest}"
+fi
+echo "Building Docker images for \"$IMAGE_PATH:$TAG\""
+docker buildx build \
+    --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6,linux/ppc64le,linux/s390x \
+    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+    --build-arg VCS_REF=`git rev-parse --short HEAD` \
+    --build-arg VERSION=$TAG \
+    -t $IMAGE_PATH:$TAG \
+    --push \
+    .
