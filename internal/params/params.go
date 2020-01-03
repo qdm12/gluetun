17e9ba444d0e11053781f7cf26c92052683f135b
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2020-01-03 01:27:25 +0000

    Unsets user and password after reading them

--- internal/params/params.go
+++ internal/params/params.go
@@ -1,11 +1,15 @@
 package params
 
-import libparams "github.com/qdm12/golibs/params"
+import (
+	"fmt"
+	"os"
 
-import "fmt"
+	libparams "github.com/qdm12/golibs/params"
+)
 
 // GetUser obtains the user to use to connect to the VPN servers
 func GetUser() (s string, err error) {
+	defer os.Unsetenv("USER")
 	s = libparams.GetEnv("USER", "")
 	if len(s) == 0 {
 		return s, fmt.Errorf("USER environment variable cannot be empty")
@@ -15,6 +19,7 @@ func GetUser() (s string, err error) {
 
 // GetPassword obtains the password to use to connect to the VPN servers
 func GetPassword() (s string, err error) {
+	defer os.Unsetenv("PASSWORD")
 	s = libparams.GetEnv("PASSWORD", "")
 	if len(s) == 0 {
 		return s, fmt.Errorf("PASSWORD environment variable cannot be empty")
