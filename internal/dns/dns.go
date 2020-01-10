7fe1428d226ea705421f75b10a1a457f90376f13
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2020-01-10 01:26:17 +0000

    DNS config builder with full test coverage

--- internal/dns/dns.go
+++ internal/dns/dns.go
@@ -2,136 +2,252 @@ package dns
 
 import (
 	"fmt"
-	"net/http"
+	"sort"
 	"strings"
-	"time"
 
-	"github.com/qdm12/golibs/logging"
 	"github.com/qdm12/golibs/network"
 	"github.com/qdm12/private-internet-access-docker/internal/constants"
 	"github.com/qdm12/private-internet-access-docker/internal/settings"
 )
 
 // MakeUnboundConf generates an Unbound configuration from the user provided settings
-func MakeUnboundConf(settings *settings.DNS) (conf string) {
-	serverSection := map[string][]string{
+func MakeUnboundConf(client network.Client, settings settings.DNS) (lines []string, errs []error) {
+	serverSection := map[string]string{
 		// Logging
-		"verbosity":     []string{fmt.Sprintf("%d", settings.Verbosity)},
-		"val-log-level": []string{fmt.Sprintf("%d", settings.LogLevel)},
-		"use-syslog":    []string{"no"},
+		"verbosity":     fmt.Sprintf("%d", settings.Verbosity),
+		"val-log-level": fmt.Sprintf("%d", settings.LogLevel),
+		"use-syslog":    "no",
 		// Performance
-		"num-threads":       []string{"1"},
-		"prefetch":          []string{"yes"},
-		"prefetch-key":      []string{"yes"},
-		"key-cache-size":    []string{"16m"},
-		"key-cache-slabs":   []string{"4"},
-		"msg-cache-size":    []string{"4m"},
-		"msg-cache-slabs":   []string{"4"},
-		"rrset-cache-size":  []string{"4m"},
-		"rrset-cache-slabs": []string{"4"},
-		"cache-min-ttl":     []string{"3600"},
-		"cache-max-ttl":     []string{"9000"},
+		"num-threads":       "1",
+		"prefetch":          "yes",
+		"prefetch-key":      "yes",
+		"key-cache-size":    "16m",
+		"key-cache-slabs":   "4",
+		"msg-cache-size":    "4m",
+		"msg-cache-slabs":   "4",
+		"rrset-cache-size":  "4m",
+		"rrset-cache-slabs": "4",
+		"cache-min-ttl":     "3600",
+		"cache-max-ttl":     "9000",
 		// Privacy
-		"rrset-roundrobin": []string{"yes"},
-		"hide-identity":    []string{"yes"},
-		"hide-version":     []string{"yes"},
+		"rrset-roundrobin": "yes",
+		"hide-identity":    "yes",
+		"hide-version":     "yes",
 		// Security
-		"tls-cert-bundle":       []string{"\"/etc/ssl/certs/ca-certificates.crt\""},
-		"root-hints":            []string{"\"/etc/unbound/root.hints\""},
-		"trust-anchor-file":     []string{"\"/etc/unbound/root.key\""},
-		"harden-below-nxdomain": []string{"yes"},
-		"harden-referral-path":  []string{"yes"},
-		"harden-algo-downgrade": []string{"yes"},
+		"tls-cert-bundle":       "\"/etc/ssl/certs/ca-certificates.crt\"",
+		"root-hints":            "\"/etc/unbound/root.hints\"",
+		"trust-anchor-file":     "\"/etc/unbound/root.key\"",
+		"harden-below-nxdomain": "yes",
+		"harden-referral-path":  "yes",
+		"harden-algo-downgrade": "yes",
 		// Network
-		"do-ip4":    []string{"yes"},
-		"do-ip6":    []string{"no"},
-		"interface": []string{"127.0.0.1"},
-		"port":      []string{"53"},
+		"do-ip4":    "yes",
+		"do-ip6":    "no",
+		"interface": "127.0.0.1",
+		"port":      "53",
 		// Other
-		"username": []string{"\"nonrootuser\""},
+		"username": "\"nonrootuser\"",
 	}
-	serverSection["private-address"] = settings.PrivateAddresses
-	forwardZoneSection := map[string][]string{
-		"name":                 []string{"\".\""},
-		"forward-tls-upstream": []string{"yes"},
-	}
-	forwardZoneSection["forward-addr"] = settings.Provider.GetForwardAddresses()
 
 	// Block lists
-	blockConf := buildBlockedHostnames(settings.BlockMalicious, settings.BlockAds, settings.BlockSurveillance)
+	blockLines, errs := buildBlocked(client,
+		settings.BlockMalicious, settings.BlockAds, settings.BlockSurveillance,
+		settings.AllowedHostnames, settings.PrivateAddresses,
+	)
 
-	// Make configuration string
-	conf = "server:\n"
-	for k, arr := range serverSection {
-		for i := range arr {
-			conf += "  " + k + ": " + arr[i] + "\n"
-		}
+	// Server
+	lines = append(lines, "server:")
+	var serverLines []string
+	for k, v := range serverSection {
+		serverLines = append(serverLines, "  "+k+": "+v)
 	}
-	conf += blockConf
-	conf += "forward-zone:\n"
-	for k, arr := range forwardZoneSection {
-		for i := range arr {
-			conf += "  " + k + ": " + arr[i] + "\n"
-		}
+	sort.Slice(serverLines, func(i, j int) bool {
+		return serverLines[i] < serverLines[j]
+	})
+	lines = append(lines, serverLines...)
+	lines = append(lines, blockLines...)
+
+	// Forward zone
+	lines = append(lines, "forward-zone:")
+	forwardZoneSection := map[string]string{
+		"name":                 "\".\"",
+		"forward-tls-upstream": "yes",
 	}
-	return conf
+	var forwardZoneLines []string
+	for k, v := range forwardZoneSection {
+		forwardZoneLines = append(forwardZoneLines, "  "+k+": "+v)
+	}
+	sort.Slice(forwardZoneLines, func(i, j int) bool {
+		return forwardZoneLines[i] < forwardZoneLines[j]
+	})
+	lines = append(lines, forwardZoneLines...)
+	for _, forwardAddress := range settings.Provider.GetForwardAddresses() {
+		lines = append(lines, "  forward-addr: "+forwardAddress)
+	}
+	return lines, errs
 }
 
-func getList(client *http.Client, URL string, chResults chan []string, chError chan error) {
-	content, err := network.GetContent(client, URL)
+func buildBlocked(client network.Client, blockMalicious, blockAds, blockSurveillance bool,
+	allowedHostnames, privateAddresses []string) (lines []string, errs []error) {
+	chResults := make(chan []string)
+	chErrors := make(chan []error)
+	go func() {
+		results, errs := buildBlockedHostnames(client, blockMalicious, blockAds, blockSurveillance, allowedHostnames)
+		chResults <- results
+		chErrors <- errs
+	}()
+	go func() {
+		results, errs := buildBlockedIPs(client, blockMalicious, blockAds, blockSurveillance, privateAddresses)
+		chResults <- results
+		chErrors <- errs
+	}()
+	n := 2
+	for n > 0 {
+		select {
+		case results := <-chResults:
+			lines = append(lines, results...)
+		case routineErrs := <-chErrors:
+			errs = append(errs, routineErrs...)
+			n--
+		}
+	}
+	sort.Slice(lines, func(i, j int) bool {
+		return lines[i] < lines[j]
+	})
+	return lines, errs
+}
+
+func getList(client network.Client, URL string) (results []string, err error) {
+	content, status, err := client.GetContent(URL)
 	if err != nil {
-		chError <- err
-		return
+		return nil, err
+	} else if status != 200 {
+		return nil, fmt.Errorf("HTTP status code is %d and not 200", status)
 	}
-	chResults <- strings.Split(string(content), "\n")
-	chError <- nil
+	results = strings.Split(string(content), "\n")
+
+	// remove empty lines
+	last := len(results) - 1
+	for i := range results {
+		if len(results[i]) == 0 {
+			results[i] = results[last]
+			last--
+		}
+	}
+	results = results[:last+1]
+
+	if len(results) == 0 {
+		return nil, nil
+	}
+	return results, nil
 }
 
-func buildBlockedHostnames(blockMalicious, blockAds, blockSurveillance bool) (conf string) {
-	client := &http.Client{Timeout: 5 * time.Second}
-	chHostnames := make(chan []string)
-	chIPs := make(chan []string)
+func buildBlockedHostnames(client network.Client, blockMalicious, blockAds, blockSurveillance bool,
+	allowedHostnames []string) (lines []string, errs []error) {
+	chResults := make(chan []string)
 	chError := make(chan error)
 	listsLeftToFetch := 0
 	if blockMalicious {
-		listsLeftToFetch += 2
-		go getList(client, constants.MaliciousBlockListHostnamesURL, chHostnames, chError)
-		go getList(client, constants.MaliciousBlockListIPsURL, chIPs, chError)
+		listsLeftToFetch++
+		go func() {
+			results, err := getList(client, constants.MaliciousBlockListHostnamesURL)
+			chResults <- results
+			chError <- err
+		}()
 	}
 	if blockAds {
-		listsLeftToFetch += 2
-		go getList(client, constants.AdsBlockListHostnamesURL, chHostnames, chError)
-		go getList(client, constants.AdsBlockListIPsURL, chIPs, chError)
+		listsLeftToFetch++
+		go func() {
+			results, err := getList(client, constants.AdsBlockListHostnamesURL)
+			chResults <- results
+			chError <- err
+		}()
 	}
 	if blockSurveillance {
-		listsLeftToFetch += 2
-		go getList(client, constants.SurveillanceBlockListHostnamesURL, chHostnames, chError)
-		go getList(client, constants.SurveillanceBlockListIPsURL, chIPs, chError)
+		listsLeftToFetch++
+		go func() {
+			results, err := getList(client, constants.SurveillanceBlockListHostnamesURL)
+			chResults <- results
+			chError <- err
+		}()
 	}
-	uniqueHostnames := make(map[string]struct{})
-	uniqueIPs := make(map[string]struct{})
+	uniqueResults := make(map[string]struct{})
 	for listsLeftToFetch > 0 {
 		select {
-		case hostnames := <-chHostnames:
-			for _, hostname := range hostnames {
-				uniqueHostnames[hostname] = struct{}{}
-			}
-		case IPs := <-chIPs:
-			for _, IP := range IPs {
-				uniqueIPs[IP] = struct{}{}
+		case results := <-chResults:
+			for _, result := range results {
+				uniqueResults[result] = struct{}{}
 			}
 		case err := <-chError:
 			listsLeftToFetch--
 			if err != nil {
-				logging.Warn(err.Error())
+				errs = append(errs, err)
 			}
 		}
 	}
-	for hostname := range uniqueHostnames {
-		conf += "local-zone: \"" + hostname + "\" static\n"
+	for _, allowedHostname := range allowedHostnames {
+		delete(uniqueResults, allowedHostname)
 	}
-	for IP := range uniqueIPs {
-		conf += "private-address: " + IP + "\n"
+	for result := range uniqueResults {
+		lines = append(lines, "  local-zone: \""+result+"\" static")
 	}
-	return conf
+	sort.Slice(lines, func(i, j int) bool {
+		return lines[i] < lines[j]
+	})
+	return lines, errs
+}
+
+func buildBlockedIPs(client network.Client, blockMalicious, blockAds, blockSurveillance bool,
+	privateAddresses []string) (lines []string, errs []error) {
+	chResults := make(chan []string)
+	chError := make(chan error)
+	listsLeftToFetch := 0
+	if blockMalicious {
+		listsLeftToFetch++
+		go func() {
+			results, err := getList(client, constants.MaliciousBlockListIPsURL)
+			chResults <- results
+			chError <- err
+		}()
+	}
+	if blockAds {
+		listsLeftToFetch++
+		go func() {
+			results, err := getList(client, constants.AdsBlockListIPsURL)
+			chResults <- results
+			chError <- err
+		}()
+	}
+	if blockSurveillance {
+		listsLeftToFetch++
+		go func() {
+			results, err := getList(client, constants.SurveillanceBlockListIPsURL)
+			chResults <- results
+			chError <- err
+		}()
+	}
+	uniqueResults := make(map[string]struct{})
+	for listsLeftToFetch > 0 {
+		select {
+		case results := <-chResults:
+			for _, result := range results {
+				uniqueResults[result] = struct{}{}
+			}
+		case err := <-chError:
+			listsLeftToFetch--
+			if err != nil {
+				errs = append(errs, err)
+			}
+		}
+	}
+	for _, privateAddress := range privateAddresses {
+		uniqueResults[privateAddress] = struct{}{}
+	}
+	for result := range uniqueResults {
+		lines = append(lines, "  private-address: "+result)
+	}
+	sort.Slice(lines, func(i, j int) bool {
+		return lines[i] < lines[j]
+	})
+	return lines, errs
 }
---
+++ internal/dns/dns_test.go
@@ -0,0 +1,504 @@
+package dns
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/qdm12/golibs/helpers"
+	"github.com/qdm12/golibs/network/mocks"
+	"github.com/qdm12/private-internet-access-docker/internal/constants"
+	"github.com/qdm12/private-internet-access-docker/internal/settings"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_MakeUnboundConf(t *testing.T) {
+	t.Parallel()
+	settings := settings.DNS{
+		Provider:          constants.Cloudflare,
+		AllowedHostnames:  []string{"a"},
+		PrivateAddresses:  []string{"9.9.9.9"},
+		BlockMalicious:    true,
+		BlockSurveillance: false,
+		BlockAds:          false,
+		Verbosity:         2,
+		LogLevel:          3,
+	}
+	client := &mocks.Client{}
+	client.On("GetContent", constants.MaliciousBlockListHostnamesURL).
+		Return([]byte("b\na"), 200, nil).Once()
+	client.On("GetContent", constants.MaliciousBlockListIPsURL).
+		Return([]byte("c\nd\n"), 200, nil).Once()
+	lines, errs := MakeUnboundConf(client, settings)
+	require.Len(t, errs, 0)
+	client.AssertExpectations(t)
+	expected := `
+server:
+  cache-max-ttl: 9000
+  cache-min-ttl: 3600
+  do-ip4: yes
+  do-ip6: no
+  harden-algo-downgrade: yes
+  harden-below-nxdomain: yes
+  harden-referral-path: yes
+  hide-identity: yes
+  hide-version: yes
+  interface: 127.0.0.1
+  key-cache-size: 16m
+  key-cache-slabs: 4
+  msg-cache-size: 4m
+  msg-cache-slabs: 4
+  num-threads: 1
+  port: 53
+  prefetch-key: yes
+  prefetch: yes
+  root-hints: "/etc/unbound/root.hints"
+  rrset-cache-size: 4m
+  rrset-cache-slabs: 4
+  rrset-roundrobin: yes
+  tls-cert-bundle: "/etc/ssl/certs/ca-certificates.crt"
+  trust-anchor-file: "/etc/unbound/root.key"
+  use-syslog: no
+  username: "nonrootuser"
+  val-log-level: 3
+  verbosity: 2
+  local-zone: "b" static
+  private-address: 9.9.9.9
+  private-address: c
+  private-address: d
+forward-zone:
+  forward-tls-upstream: yes
+  name: "."
+  forward-addr: 1.1.1.1@853#cloudflare-dns.com
+  forward-addr: 1.0.0.1@853#cloudflare-dns.com`
+	assert.Equal(t, expected, "\n"+strings.Join(lines, "\n"))
+}
+
+func Test_buildBlocked(t *testing.T) {
+	t.Parallel()
+	type blockParams struct {
+		blocked   bool
+		content   []byte
+		clientErr error
+	}
+	tests := map[string]struct {
+		malicious        blockParams
+		ads              blockParams
+		surveillance     blockParams
+		allowedHostnames []string
+		privateAddresses []string
+		lines            []string
+		errsString       []string
+	}{
+		"none blocked": {},
+		"all blocked without lists": {
+			malicious: blockParams{
+				blocked: true,
+			},
+			ads: blockParams{
+				blocked: true,
+			},
+			surveillance: blockParams{
+				blocked: true,
+			},
+		},
+		"all blocked with lists": {
+			malicious: blockParams{
+				blocked: true,
+				content: []byte("malicious"),
+			},
+			ads: blockParams{
+				blocked: true,
+				content: []byte("ads"),
+			},
+			surveillance: blockParams{
+				blocked: true,
+				content: []byte("surveillance"),
+			},
+			lines: []string{
+				"  local-zone: \"ads\" static",
+				"  local-zone: \"malicious\" static",
+				"  local-zone: \"surveillance\" static",
+				"  private-address: ads",
+				"  private-address: malicious",
+				"  private-address: surveillance",
+			},
+		},
+		"all blocked with allowed hostnames": {
+			malicious: blockParams{
+				blocked: true,
+				content: []byte("malicious"),
+			},
+			ads: blockParams{
+				blocked: true,
+				content: []byte("ads"),
+			},
+			surveillance: blockParams{
+				blocked: true,
+				content: []byte("surveillance"),
+			},
+			allowedHostnames: []string{"ads"},
+			lines: []string{
+				"  local-zone: \"malicious\" static",
+				"  local-zone: \"surveillance\" static",
+				"  private-address: ads",
+				"  private-address: malicious",
+				"  private-address: surveillance",
+			},
+		},
+		"all blocked with private addresses": {
+			malicious: blockParams{
+				blocked: true,
+				content: []byte("malicious"),
+			},
+			ads: blockParams{
+				blocked: true,
+				content: []byte("ads"),
+			},
+			surveillance: blockParams{
+				blocked: true,
+				content: []byte("surveillance"),
+			},
+			privateAddresses: []string{"ads", "192.100.1.5"},
+			lines: []string{
+				"  local-zone: \"ads\" static",
+				"  local-zone: \"malicious\" static",
+				"  local-zone: \"surveillance\" static",
+				"  private-address: 192.100.1.5",
+				"  private-address: ads",
+				"  private-address: malicious",
+				"  private-address: surveillance",
+			},
+		},
+		"all blocked with lists and one error": {
+			malicious: blockParams{
+				blocked: true,
+				content: []byte("malicious"),
+			},
+			ads: blockParams{
+				blocked:   true,
+				content:   []byte("ads"),
+				clientErr: fmt.Errorf("ads error"),
+			},
+			surveillance: blockParams{
+				blocked: true,
+				content: []byte("surveillance"),
+			},
+			lines: []string{
+				"  local-zone: \"malicious\" static",
+				"  local-zone: \"surveillance\" static",
+				"  private-address: malicious",
+				"  private-address: surveillance",
+			},
+			errsString: []string{"ads error", "ads error"},
+		},
+		"all blocked with errors": {
+			malicious: blockParams{
+				blocked:   true,
+				clientErr: fmt.Errorf("malicious"),
+			},
+			ads: blockParams{
+				blocked:   true,
+				clientErr: fmt.Errorf("ads"),
+			},
+			surveillance: blockParams{
+				blocked:   true,
+				clientErr: fmt.Errorf("surveillance"),
+			},
+			errsString: []string{"malicious", "malicious", "ads", "ads", "surveillance", "surveillance"},
+		},
+	}
+	for name, tc := range tests {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			client := &mocks.Client{}
+			if tc.malicious.blocked {
+				client.On("GetContent", constants.MaliciousBlockListHostnamesURL).
+					Return(tc.malicious.content, 200, tc.malicious.clientErr).Once()
+				client.On("GetContent", constants.MaliciousBlockListIPsURL).
+					Return(tc.malicious.content, 200, tc.malicious.clientErr).Once()
+			}
+			if tc.ads.blocked {
+				client.On("GetContent", constants.AdsBlockListHostnamesURL).
+					Return(tc.ads.content, 200, tc.ads.clientErr).Once()
+				client.On("GetContent", constants.AdsBlockListIPsURL).
+					Return(tc.ads.content, 200, tc.ads.clientErr).Once()
+			}
+			if tc.surveillance.blocked {
+				client.On("GetContent", constants.SurveillanceBlockListHostnamesURL).
+					Return(tc.surveillance.content, 200, tc.surveillance.clientErr).Once()
+				client.On("GetContent", constants.SurveillanceBlockListIPsURL).
+					Return(tc.surveillance.content, 200, tc.surveillance.clientErr).Once()
+			}
+			lines, errs := buildBlocked(client, tc.malicious.blocked, tc.ads.blocked, tc.surveillance.blocked,
+				tc.allowedHostnames, tc.privateAddresses)
+			var errsString []string
+			for _, err := range errs {
+				errsString = append(errsString, err.Error())
+			}
+			assert.ElementsMatch(t, tc.errsString, errsString)
+			assert.Equal(t, tc.lines, lines)
+			client.AssertExpectations(t)
+		})
+	}
+}
+
+func Test_getList(t *testing.T) {
+	t.Parallel()
+	tests := map[string]struct {
+		content   []byte
+		status    int
+		clientErr error
+		results   []string
+		err       error
+	}{
+		"no result":     {nil, 200, nil, nil, nil},
+		"bad status":    {nil, 500, nil, nil, fmt.Errorf("HTTP status code is 500 and not 200")},
+		"network error": {nil, 200, fmt.Errorf("error"), nil, fmt.Errorf("error")},
+		"results":       {[]byte("a\nb\nc\n"), 200, nil, []string{"a", "b", "c"}, nil},
+	}
+	for name, tc := range tests {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			client := &mocks.Client{}
+			client.On("GetContent", "irrelevant_url").Return(
+				tc.content, tc.status, tc.clientErr,
+			).Once()
+			results, err := getList(client, "irrelevant_url")
+			helpers.AssertErrorsEqual(t, tc.err, err)
+			assert.Equal(t, tc.results, results)
+			client.AssertExpectations(t)
+		})
+	}
+}
+
+func Test_buildBlockedHostnames(t *testing.T) {
+	t.Parallel()
+	type blockParams struct {
+		blocked   bool
+		content   []byte
+		clientErr error
+	}
+	tests := map[string]struct {
+		malicious        blockParams
+		ads              blockParams
+		surveillance     blockParams
+		allowedHostnames []string
+		lines            []string
+		errsString       []string
+	}{
+		"nothing blocked": {
+			lines:      nil,
+			errsString: nil,
+		},
+		"only malicious blocked": {
+			malicious: blockParams{
+				blocked:   true,
+				content:   []byte("site_a\nsite_b"),
+				clientErr: nil,
+			},
+			lines: []string{
+				"  local-zone: \"site_a\" static",
+				"  local-zone: \"site_b\" static"},
+			errsString: nil,
+		},
+		"all blocked with some duplicates": {
+			malicious: blockParams{
+				blocked: true,
+				content: []byte("site_a\nsite_b"),
+			},
+			ads: blockParams{
+				blocked: true,
+				content: []byte("site_a\nsite_c"),
+			},
+			surveillance: blockParams{
+				blocked: true,
+				content: []byte("site_c\nsite_a"),
+			},
+			lines: []string{
+				"  local-zone: \"site_a\" static",
+				"  local-zone: \"site_b\" static",
+				"  local-zone: \"site_c\" static"},
+			errsString: nil,
+		},
+		"all blocked with one errored": {
+			malicious: blockParams{
+				blocked: true,
+				content: []byte("site_a\nsite_b"),
+			},
+			ads: blockParams{
+				blocked: true,
+				content: []byte("site_a\nsite_c"),
+			},
+			surveillance: blockParams{
+				blocked:   true,
+				clientErr: fmt.Errorf("surveillance error"),
+			},
+			lines: []string{
+				"  local-zone: \"site_a\" static",
+				"  local-zone: \"site_b\" static",
+				"  local-zone: \"site_c\" static"},
+			errsString: []string{"surveillance error"},
+		},
+		"blocked with allowed hostnames": {
+			malicious: blockParams{
+				blocked: true,
+				content: []byte("site_a\nsite_b"),
+			},
+			ads: blockParams{
+				blocked: true,
+				content: []byte("site_c\nsite_d"),
+			},
+			allowedHostnames: []string{"site_b", "site_c"},
+			lines: []string{
+				"  local-zone: \"site_a\" static",
+				"  local-zone: \"site_d\" static"},
+		},
+	}
+	for name, tc := range tests {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			client := &mocks.Client{}
+			if tc.malicious.blocked {
+				client.On("GetContent", constants.MaliciousBlockListHostnamesURL).
+					Return(tc.malicious.content, 200, tc.malicious.clientErr).Once()
+			}
+			if tc.ads.blocked {
+				client.On("GetContent", constants.AdsBlockListHostnamesURL).
+					Return(tc.ads.content, 200, tc.ads.clientErr).Once()
+			}
+			if tc.surveillance.blocked {
+				client.On("GetContent", constants.SurveillanceBlockListHostnamesURL).
+					Return(tc.surveillance.content, 200, tc.surveillance.clientErr).Once()
+			}
+			lines, errs := buildBlockedHostnames(client,
+				tc.malicious.blocked, tc.ads.blocked, tc.surveillance.blocked, tc.allowedHostnames)
+			var errsString []string
+			for _, err := range errs {
+				errsString = append(errsString, err.Error())
+			}
+			assert.ElementsMatch(t, tc.errsString, errsString)
+			assert.Equal(t, tc.lines, lines)
+			client.AssertExpectations(t)
+		})
+	}
+}
+
+func Test_buildBlockedIPs(t *testing.T) {
+	t.Parallel()
+	type blockParams struct {
+		blocked   bool
+		content   []byte
+		clientErr error
+	}
+	tests := map[string]struct {
+		malicious        blockParams
+		ads              blockParams
+		surveillance     blockParams
+		privateAddresses []string
+		lines            []string
+		errsString       []string
+	}{
+		"nothing blocked": {
+			lines:      nil,
+			errsString: nil,
+		},
+		"only malicious blocked": {
+			malicious: blockParams{
+				blocked:   true,
+				content:   []byte("site_a\nsite_b"),
+				clientErr: nil,
+			},
+			lines: []string{
+				"  private-address: site_a",
+				"  private-address: site_b"},
+			errsString: nil,
+		},
+		"all blocked with some duplicates": {
+			malicious: blockParams{
+				blocked: true,
+				content: []byte("site_a\nsite_b"),
+			},
+			ads: blockParams{
+				blocked: true,
+				content: []byte("site_a\nsite_c"),
+			},
+			surveillance: blockParams{
+				blocked: true,
+				content: []byte("site_c\nsite_a"),
+			},
+			lines: []string{
+				"  private-address: site_a",
+				"  private-address: site_b",
+				"  private-address: site_c"},
+			errsString: nil,
+		},
+		"all blocked with one errored": {
+			malicious: blockParams{
+				blocked: true,
+				content: []byte("site_a\nsite_b"),
+			},
+			ads: blockParams{
+				blocked: true,
+				content: []byte("site_a\nsite_c"),
+			},
+			surveillance: blockParams{
+				blocked:   true,
+				clientErr: fmt.Errorf("surveillance error"),
+			},
+			lines: []string{
+				"  private-address: site_a",
+				"  private-address: site_b",
+				"  private-address: site_c"},
+			errsString: []string{"surveillance error"},
+		},
+		"blocked with private addresses": {
+			malicious: blockParams{
+				blocked: true,
+				content: []byte("site_a\nsite_b"),
+			},
+			ads: blockParams{
+				blocked: true,
+				content: []byte("site_c"),
+			},
+			privateAddresses: []string{"site_c", "site_d"},
+			lines: []string{
+				"  private-address: site_a",
+				"  private-address: site_b",
+				"  private-address: site_c",
+				"  private-address: site_d"},
+		},
+	}
+	for name, tc := range tests {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			client := &mocks.Client{}
+			if tc.malicious.blocked {
+				client.On("GetContent", constants.MaliciousBlockListIPsURL).
+					Return(tc.malicious.content, 200, tc.malicious.clientErr).Once()
+			}
+			if tc.ads.blocked {
+				client.On("GetContent", constants.AdsBlockListIPsURL).
+					Return(tc.ads.content, 200, tc.ads.clientErr).Once()
+			}
+			if tc.surveillance.blocked {
+				client.On("GetContent", constants.SurveillanceBlockListIPsURL).
+					Return(tc.surveillance.content, 200, tc.surveillance.clientErr).Once()
+			}
+			lines, errs := buildBlockedIPs(client,
+				tc.malicious.blocked, tc.ads.blocked, tc.surveillance.blocked, tc.privateAddresses)
+			var errsString []string
+			for _, err := range errs {
+				errsString = append(errsString, err.Error())
+			}
+			assert.ElementsMatch(t, tc.errsString, errsString)
+			assert.Equal(t, tc.lines, lines)
+			client.AssertExpectations(t)
+		})
+	}
+}
