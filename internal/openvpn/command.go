e346d3fec23128d80992c4c195e74b6126782b95
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2020-02-02 18:06:07 +0000

    Openvpn commander missing methods

---
+++ internal/openvpn/command.go
@@ -0,0 +1,27 @@
+package openvpn
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/qdm12/private-internet-access-docker/internal/constants"
+)
+
+func (c *configurator) Start() (stdout io.ReadCloser, err error) {
+	stdout, _, err = c.commander.Start("openvpn", "--config", string(constants.OpenVPNConf))
+	return stdout, err
+}
+
+func (c *configurator) Version() (string, error) {
+	output, err := c.commander.Run("openvpn", "--version")
+	if err != nil {
+		return "", err
+	}
+	firstLine := strings.Split(output, "\n")[0]
+	words := strings.Split(firstLine, " ")
+	if len(words) < 2 {
+		return "", fmt.Errorf("openvpn --version: first line is too short: %q", firstLine)
+	}
+	return words[1], nil
+}
