9239e840c41927cb830b945d9124159b0edaa918
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2020-02-23 19:11:03 -0500

    Fixed CI script for pull requests

--- ci.sh
+++ ci.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
-  echo "Building pull request without pushing to Docker Hub"
+echo -e "\n\nPull request: $TRAVIS_PULL_REQUEST\nRelease tag: $TRAVIS_TAG\nBranch: $TRAVIS_BRANCH\n\n"
+
+if [ ! -z "$TRAVIS_PULL_REQUEST" ]; then
+  echo -e "\n\nBuilding pull request without pushing to Docker Hub\n\n"
   docker buildx build \
     --progress plain \
     --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6,linux/ppc64le,linux/s390x \
     .
   exit $?
 fi
-echo $DOCKER_PASSWORD | docker login -u qmcgaw --password-stdin &> /dev/null
+
+echo $DOCKER_PASSWORD | docker login -u qmcgaw --password-stdin 2>&1 | tail -n 1
+
 TAG="$TRAVIS_TAG"
 if [ -z $TAG ]; then
   if [ "$TRAVIS_BRANCH" = "master" ]; then
@@ -18,7 +22,7 @@ if [ -z $TAG ]; then
   fi
 fi
 
-echo "Building Docker images for \"$DOCKER_REPO:$TAG\""
+echo -e "\n\nBuilding Docker images for \"$DOCKER_REPO:$TAG\"\n\n"
 docker buildx build \
     --progress plain \
     --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6,linux/ppc64le,linux/s390x \
