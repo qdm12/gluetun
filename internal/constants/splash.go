318c3c90329e011f93821ebeb13a5ef06de1354d
Quentin McGaw <quentin.mcgaw@gmail.com>
2020-05-02 17:02:39 +0000

    Control server announcement

--- internal/constants/splash.go
+++ internal/constants/splash.go
@@ -2,9 +2,9 @@ package constants
 
 const (
 	// Announcement is a message announcement
-	Announcement = "Support for Windscribe"
+	Announcement = "New HTTP control server, see https://github.com/qdm12/private-internet-access-docker#http-control-server"
 	// AnnouncementExpiration is the expiration date of the announcement in format yyyy-mm-dd
-	AnnouncementExpiration = "2020-04-15"
+	AnnouncementExpiration = "2020-05-20"
 )
 
 const (
