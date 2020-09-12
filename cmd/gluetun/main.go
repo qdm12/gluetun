20492620b0e67d23ab6a2925b48074e7a6a45222
Quentin McGaw <quentin.mcgaw@gmail.com>
2020-09-12 15:36:12 +0000

    Using DNS over TLS to update servers

--- cmd/gluetun/main.go
+++ cmd/gluetun/main.go
@@ -197,7 +197,7 @@ func _main(background context.Context, args []string) int { //nolint:gocognit,go
 	// wait for restartOpenvpn
 	go openvpnLooper.Run(ctx, wg)
 
-	updaterOptions := updater.NewOptions(allSettings.DNS.PlaintextAddress.String())
+	updaterOptions := updater.NewOptions("127.0.0.1")
 	updaterLooper := updater.NewLooper(updaterOptions, allSettings.UpdaterPeriod, allServers, storage, openvpnLooper.SetAllServers, httpClient, logger)
 	wg.Add(1)
 	// wait for updaterLooper.Restart() or its ticket launched with RunRestartTicker
