cd3721b5eec0807e209d8d71343a2450a478f79b
Quentin McGaw <quentin.mcgaw@gmail.com>
2020-09-18 17:42:57 +0000

    Asynchronous repeatResolve

--- internal/updater/resolver.go
+++ internal/updater/resolver.go
@@ -1,8 +1,10 @@
 package updater
 
 import (
+	"bytes"
 	"context"
 	"net"
+	"sort"
 )
 
 func newResolver(resolverAddress string) *net.Resolver {
@@ -30,12 +32,49 @@ func newLookupIP(r *net.Resolver) lookupIPFunc {
 }
 
 func resolveRepeat(ctx context.Context, lookupIP lookupIPFunc, host string, n int) (ips []net.IP, err error) {
+	foundIPs := make(chan []net.IP)
+	errors := make(chan error)
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
 	for i := 0; i < n; i++ {
-		newIPs, err := lookupIP(ctx, host)
-		if err != nil {
-			return nil, err
-		}
-		ips = append(ips, newIPs...)
+		go func() {
+			newIPs, err := lookupIP(ctx, host)
+			if err != nil {
+				errors <- err
+			} else {
+				foundIPs <- newIPs
+			}
+		}()
 	}
-	return uniqueSortedIPs(ips), nil
+
+	uniqueIPs := make(map[string]struct{})
+	for i := 0; i < n; i++ {
+		select {
+		case newIPs := <-foundIPs:
+			for _, ip := range newIPs {
+				key := ip.String()
+				uniqueIPs[key] = struct{}{}
+			}
+		case newErr := <-errors:
+			if err == nil {
+				err = newErr
+				cancel()
+			}
+		}
+	}
+
+	ips = make([]net.IP, 0, len(uniqueIPs))
+	for key := range uniqueIPs {
+		ip := net.ParseIP(key)
+		if ipv4 := ip.To4(); ipv4 != nil {
+			ip = ipv4
+		}
+		ips = append(ips, ip)
+	}
+
+	sort.Slice(ips, func(i, j int) bool {
+		return bytes.Compare(ips[i], ips[j]) < 1
+	})
+
+	return ips, err
 }
