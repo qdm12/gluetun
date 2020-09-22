e9598095d546c39a47c200eec925cf179ce32ac0
Quentin McGaw <quentin.mcgaw@gmail.com>
2020-09-22 12:25:45 +0000

    Not implemented port forwarding for v4

--- internal/provider/piav4.go
+++ internal/provider/piav4.go
@@ -28,27 +28,7 @@ func (p *piaV4) GetOpenVPNConnections(selection models.ServerSelection) (connect
 func (p *piaV4) BuildConf(connections []models.OpenVPNConnection, verbosity, uid, gid int, root bool, cipher, auth string, extras models.ExtraConfigOptions) (lines []string) {
 	return buildPIAConf(connections, verbosity, root, cipher, auth, extras)
 }
+
 func (p *piaV4) GetPortForward(client network.Client) (port uint16, err error) {
-	b, err := p.random.GenerateRandomBytes(32)
-	if err != nil {
-		return 0, err
-	}
-	clientID := hex.EncodeToString(b)
-	url := fmt.Sprintf("%s/?client_id=%s", constants.PIAPortForwardURL, clientID)
-	content, status, err := client.GetContent(url) // TODO add ctx
-	switch {
-	case err != nil:
-		return 0, err
-	case status != http.StatusOK:
-		return 0, fmt.Errorf("status is %d for %s; does your PIA server support port forwarding?", status, url)
-	case len(content) == 0:
-		return 0, fmt.Errorf("port forwarding is already activated on this connection, has expired, or you are not connected to a PIA region that supports port forwarding")
-	}
-	body := struct {
-		Port uint16 `json:"port"`
-	}{}
-	if err := json.Unmarshal(content, &body); err != nil {
-		return 0, fmt.Errorf("port forwarding response: %w", err)
-	}
-	return body.Port, nil
+	return 0, fmt.Errorf("not implemented")
 }
