c69f1d7f1337c1bdd02192a8750991f293c991d3
Quentin McGaw <quentin.mcgaw@gmail.com>
2020-10-08 00:35:51 +0000

    Adapt PIA v3 port forwarding

--- internal/provider/piav3.go
+++ internal/provider/piav3.go
@@ -1,15 +1,20 @@
 package provider
 
 import (
+	"context"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
 
 	"github.com/qdm12/gluetun/internal/constants"
+	"github.com/qdm12/gluetun/internal/firewall"
 	"github.com/qdm12/gluetun/internal/models"
 	"github.com/qdm12/golibs/crypto/random"
+	"github.com/qdm12/golibs/files"
+	"github.com/qdm12/golibs/logging"
 )
 
 type piaV3 struct {
@@ -32,32 +37,58 @@ func (p *piaV3) BuildConf(connections []models.OpenVPNConnection, verbosity, uid
 	return buildPIAConf(connections, verbosity, root, cipher, auth, extras)
 }
 
-func (p *piaV3) GetPortForward(client *http.Client) (port uint16, err error) {
+func (p *piaV3) PortForward(ctx context.Context, client *http.Client,
+	fileManager files.FileManager, pfLogger logging.Logger, gateway net.IP, fw firewall.Configurator,
+	syncState func(port uint16) (pfFilepath models.Filepath)) {
 	b, err := p.random.GenerateRandomBytes(32)
 	if err != nil {
-		return 0, err
+		pfLogger.Error(err)
+		return
 	}
 	clientID := hex.EncodeToString(b)
 	url := fmt.Sprintf("%s/?client_id=%s", constants.PIAPortForwardURL, clientID)
 	response, err := client.Get(url) // TODO add ctx
 	if err != nil {
-		return 0, err
+		pfLogger.Error(err)
+		return
 	}
 	defer response.Body.Close()
 	if response.StatusCode != http.StatusOK {
-		return 0, fmt.Errorf("%s for %s; does your PIA server support port forwarding?", response.Status, url)
+		pfLogger.Error(fmt.Errorf("%s for %s; does your PIA server support port forwarding?", response.Status, url))
+		return
 	}
 	b, err = ioutil.ReadAll(response.Body)
 	if err != nil {
-		return 0, err
+		pfLogger.Error(err)
+		return
 	} else if len(b) == 0 {
-		return 0, fmt.Errorf("port forwarding is already activated on this connection, has expired, or you are not connected to a PIA region that supports port forwarding")
+		pfLogger.Error(fmt.Errorf("port forwarding is already activated on this connection, has expired, or you are not connected to a PIA region that supports port forwarding"))
+		return
 	}
 	body := struct {
 		Port uint16 `json:"port"`
 	}{}
 	if err := json.Unmarshal(b, &body); err != nil {
-		return 0, fmt.Errorf("port forwarding response: %w", err)
+		pfLogger.Error(fmt.Errorf("port forwarding response: %w", err))
+		return
 	}
-	return body.Port, nil
+	port := body.Port
+
+	filepath := syncState(port)
+	pfLogger.Info("Writing port to %s", filepath)
+	if err := fileManager.WriteToFile(
+		string(filepath), []byte(fmt.Sprintf("%d", port)),
+		files.Permissions(0666),
+	); err != nil {
+		pfLogger.Error(err)
+	}
+
+	if err := fw.SetAllowedPort(ctx, port, string(constants.TUN)); err != nil {
+		pfLogger.Error(err)
+	}
+
+	<-ctx.Done()
+	if err := fw.RemoveAllowedPort(ctx, port); err != nil {
+		pfLogger.Error(err)
+	}
 }
