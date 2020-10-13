30ba72c2e09beaa011987007e93b52435de8f9ba
Quentin McGaw <quentin.mcgaw@gmail.com>
2020-10-13 13:14:04 +0000

    Disable TLS verification

--- internal/provider/piav4.go
+++ internal/provider/piav4.go
@@ -126,7 +126,7 @@ func (p *piaV4) PortForward(ctx context.Context, client *http.Client,
 	if p.activeProtocol == constants.TCP {
 		commonName = p.activeServer.OpenvpnTCP.CN
 	}
-	client, err := newPIAv4HTTPClient(commonName)
+	client, err := newPIAv4HTTPClient(commonName, gateway)
 	if err != nil {
 		pfLogger.Error("aborting because: %s", err)
 		return
@@ -249,7 +249,7 @@ func filterPIAServers(servers []models.PIAServer, region string) (filtered []mod
 	return nil
 }
 
-func newPIAv4HTTPClient(commonName string) (client *http.Client, err error) {
+func newPIAv4HTTPClient(commonName string, gateway net.IP) (client *http.Client, err error) {
 	certificateBytes, err := base64.StdEncoding.DecodeString(constants.PIACertificateStrong)
 	if err != nil {
 		return nil, fmt.Errorf("cannot decode PIA root certificate: %w", err)
@@ -261,12 +261,14 @@ func newPIAv4HTTPClient(commonName string) (client *http.Client, err error) {
 	certificate.Subject.CommonName = commonName
 	certificate.Issuer.CommonName = commonName
 	certificate.DNSNames = []string{commonName}
-	certificate.IPAddresses = []net.IP{{10, 0, 0, 1}}
+	certificate.IPAddresses = []net.IP{{10, 0, 0, 1}, gateway}
 	rootCAs := x509.NewCertPool()
 	rootCAs.AddCert(certificate)
 	TLSClientConfig := &tls.Config{
 		RootCAs:    rootCAs,
 		MinVersion: tls.VersionTLS12,
+		// TODO disable once SAN is added to PIA servers certificates, see https://github.com/pia-foss/manual-connections/issues/10
+		InsecureSkipVerify: true, //nolint:gosec
 	}
 	transport := http.Transport{
 		TLSClientConfig: TLSClientConfig,
