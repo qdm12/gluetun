0b985e8c353fd772c202f5a8b0a6a46cdae65ab3
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2021-07-23 18:51:51 +0000

    Maint: alpine package interface rework - return concrete struct type - split interface is sub-interfaces

--- internal/alpine/alpine.go
+++ internal/alpine/alpine.go
@@ -2,24 +2,25 @@
 package alpine
 
 import (
-	"context"
 	"os/user"
 )
 
-type Configurator interface {
-	CreateUser(username string, uid int) (createdUsername string, err error)
-	Version(ctx context.Context) (version string, err error)
+var _ Alpiner = (*Alpine)(nil)
+
+type Alpiner interface {
+	UserCreater
+	VersionGetter
 }
 
-type configurator struct {
+type Alpine struct {
 	alpineReleasePath string
 	passwdPath        string
 	lookupID          func(uid string) (*user.User, error)
 	lookup            func(username string) (*user.User, error)
 }
 
-func NewConfigurator() Configurator {
-	return &configurator{
+func New() *Alpine {
+	return &Alpine{
 		alpineReleasePath: "/etc/alpine-release",
 		passwdPath:        "/etc/passwd",
 		lookupID:          user.LookupId,
