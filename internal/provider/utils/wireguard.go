2dcd5c220b74c848f2462fb5a64b1ec26a23eec8
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2021-08-19 15:32:48 +0000

    Wireguard settings builder from connection + user settings

---
+++ internal/provider/utils/wireguard.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"net"
+
+	"github.com/qdm12/gluetun/internal/configuration"
+	"github.com/qdm12/gluetun/internal/models"
+	"github.com/qdm12/gluetun/internal/wireguard"
+)
+
+func BuildWireguardSettings(connection models.Connection,
+	userSettings configuration.Wireguard) (settings wireguard.Settings) {
+	settings.PrivateKey = userSettings.PrivateKey
+	settings.PublicKey = connection.PubKey
+	settings.PreSharedKey = userSettings.PreSharedKey
+
+	settings.Endpoint = new(net.UDPAddr)
+	settings.Endpoint.IP = make(net.IP, len(connection.IP))
+	copy(settings.Endpoint.IP, connection.IP)
+	settings.Endpoint.Port = int(connection.Port)
+
+	address := new(net.IPNet)
+	address.IP = make(net.IP, len(userSettings.Address.IP))
+	copy(address.IP, userSettings.Address.IP)
+	address.Mask = make(net.IPMask, len(userSettings.Address.Mask))
+	copy(address.Mask, userSettings.Address.Mask)
+
+	return settings
+}
