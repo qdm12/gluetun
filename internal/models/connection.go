dc402bc443c6b01f1e46aa3356dde87336cf372d
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2021-08-19 15:32:15 +0000

    Make connection model work with Wireguard - Add PubKey field - Update comments on fields

--- internal/models/connection.go
+++ internal/models/connection.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Connection struct {
-	// Type is the connection type and can be "openvpn"
+	// Type is the connection type and can be "openvpn" or "wireguard"
 	Type string `json:"type"`
 	// IP is the VPN server IP address.
 	IP net.IP `json:"ip"`
@@ -15,13 +15,17 @@ type Connection struct {
 	// Protocol can be "tcp" or "udp".
 	Protocol string `json:"protocol"`
 	// Hostname is used for IPVanish, IVPN, Privado
-	// and Windscribe for TLS verification
+	// and Windscribe for TLS verification.
 	Hostname string `json:"hostname"`
+	// PubKey is the public key of the VPN server,
+	// used only for Wireguard.
+	PubKey string `json:"pubkey"`
 }
 
 func (c *Connection) Equal(other Connection) bool {
 	return c.IP.Equal(other.IP) && c.Port == other.Port &&
-		c.Protocol == other.Protocol && c.Hostname == other.Hostname
+		c.Protocol == other.Protocol && c.Hostname == other.Hostname &&
+		c.PubKey == other.PubKey
 }
 
 func (c Connection) OpenVPNRemoteLine() (line string) {
@@ -43,5 +47,8 @@ func (c *Connection) UpdateEmptyWith(ip net.IP, port uint16, protocol string) {
 	}
 	if c.Protocol == "" {
 		c.Protocol = protocol
+	}
+	if c.PubKey == "" {
+		c.PubKey = connection.PubKey
 	}
 }
