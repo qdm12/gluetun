4b1a742d312cd4151102eddf12539280d38dca1f
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2021-08-23 15:45:28 +0000

    Server filtering and port/protocol determination

--- internal/updater/providers/ivpn/sort.go
+++ internal/updater/providers/ivpn/sort.go
@@ -10,6 +10,9 @@ func sortServers(servers []models.IvpnServer) {
 	sort.Slice(servers, func(i, j int) bool {
 		if servers[i].Country == servers[j].Country {
 			if servers[i].City == servers[j].City {
+				if servers[i].Hostname == servers[j].Hostname {
+					return servers[i].VPN < servers[j].VPN
+				}
 				return servers[i].Hostname < servers[j].Hostname
 			}
 			return servers[i].City < servers[j].City
