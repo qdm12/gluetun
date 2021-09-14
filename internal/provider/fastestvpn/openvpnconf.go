b872973e8bf0b3cf6bb83e3a591e6fe778b3caab
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2021-09-14 15:04:50 +0000

    Maint: OpenVPN option `tls-client` removed - It's redundant with `client` option - Affects FastestVPN

--- internal/provider/fastestvpn/openvpnconf.go
+++ internal/provider/fastestvpn/openvpnconf.go
@@ -31,7 +31,6 @@ func (f *Fastestvpn) BuildConf(connection models.Connection,
 
 		// Fastestvpn specific
 		"ping-restart 0",
-		"tls-client",
 		"tls-cipher  TLS-DHE-RSA-WITH-AES-256-GCM-SHA384:TLS-DHE-RSA-WITH-AES-256-CBC-SHA256:TLS-DHE-RSA-WITH-CAMELLIA-256-CBC-SHA:TLS-DHE-RSA-WITH-AES-256-CBC-SHA:TLS-RSA-WITH-CAMELLIA-256-CBC-SHA:TLS-RSA-WITH-AES-256-CBC-SHA", //nolint:lll
 		"key-direction 1",
 		"tun-mtu-extra 32",
