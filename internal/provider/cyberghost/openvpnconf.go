80f25c34e5bbc0aada856e6ce4f62e20acd08633
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2021-09-15 12:54:36 +0000

    Hotfix: default cyberghost cipher to aes-128-gcm

--- internal/provider/cyberghost/openvpnconf.go
+++ internal/provider/cyberghost/openvpnconf.go
@@ -13,7 +13,7 @@ import (
 func (c *Cyberghost) BuildConf(connection models.Connection,
 	settings configuration.OpenVPN) (lines []string, err error) {
 	if settings.Cipher == "" {
-		settings.Cipher = constants.AES256cbc
+		settings.Cipher = constants.AES128gcm
 	}
 
 	if settings.Auth == "" {
