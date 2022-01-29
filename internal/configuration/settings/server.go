53952b143f954a342cb81a70c488e9b3b9e0927d
Quentin McGaw <quentin.mcgaw@gmail.com>
2022-01-29 14:19:02 +0000

    fix(server): allow to bind on a random port

--- internal/configuration/settings/server.go
+++ internal/configuration/settings/server.go
@@ -33,7 +33,7 @@ func (c ControlServer) validate() (err error) {
 
 	uid := os.Getuid()
 	const maxPrivilegedPort = 1023
-	if uid != 0 && port <= maxPrivilegedPort {
+	if uid != 0 && port != 0 && port <= maxPrivilegedPort {
 		return fmt.Errorf("%w: %d when running with user ID %d",
 			ErrControlServerPrivilegedPort, port, uid)
 	}
