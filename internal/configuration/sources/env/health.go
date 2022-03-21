5cc55c92fae115e3d4ec0f0ef554bd9ec167fcd8
Quentin McGaw <quentin.mcgaw@gmail.com>
2022-03-21 20:56:33 +0000

    fix(health): use TCP dialing instead of ping - `HEALTH_TARGET_ADDRESS` to replace `HEALTH_ADDRESS_TO_PING` - Remove `github.com/go-ping/ping` dependency - Dial TCP the target address, appending `:443` if port is not set

--- internal/configuration/sources/env/health.go
+++ internal/configuration/sources/env/health.go
@@ -10,7 +10,10 @@ import (
 
 func (r *Reader) ReadHealth() (health settings.Health, err error) {
 	health.ServerAddress = os.Getenv("HEALTH_SERVER_ADDRESS")
-	health.AddressToPing = os.Getenv("HEALTH_ADDRESS_TO_PING")
+	health.TargetAddress = os.Getenv("HEALTH_ADDRESS_TO_PING")
+	if health.TargetAddress == "" {
+		health.TargetAddress = os.Getenv("HEALTH_TARGET_ADDRESS")
+	}
 
 	health.VPN.Initial, err = r.readDurationWithRetro(
 		"HEALTH_VPN_DURATION_INITIAL",
