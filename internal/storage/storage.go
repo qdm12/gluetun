381089ebdf53e95df3b2967e121651f9f5316e8d
Quentin McGaw <quentin.mcgaw@gmail.com>
2022-05-28 16:05:19 +0000

    chore(storage): rename `InfoErrorer` to `Infoer` (bad name)

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -10,18 +10,18 @@ import (
 type Storage struct {
 	mergedServers    models.AllServers
 	hardcodedServers models.AllServers
-	logger           InfoErrorer
+	logger           Infoer
 	filepath         string
 }
 
-type InfoErrorer interface {
+type Infoer interface {
 	Info(s string)
 }
 
 // New creates a new storage and reads the servers from the
 // embedded servers file and the file on disk.
 // Passing an empty filepath disables writing servers to a file.
-func New(logger InfoErrorer, filepath string) (storage *Storage, err error) {
+func New(logger Infoer, filepath string) (storage *Storage, err error) {
 	// error returned covered by unit test
 	harcodedServers, _ := parseHardcodedServers()
 
