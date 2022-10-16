ed72612a2e4bafce8942d661f9835a5a3ec01857
Quentin McGaw <quentin.mcgaw@gmail.com>
2022-10-16 07:30:34 +0000

    Add all ciphers depending on OpenVPN version

--- internal/constants/openvpn/ciphers.go
+++ internal/constants/openvpn/ciphers.go
@@ -1,8 +1,11 @@
 package openvpn
 
 const (
-	AES128cbc = "aes-128-cbc"
-	AES256cbc = "aes-256-cbc"
-	AES128gcm = "aes-128-gcm"
-	AES256gcm = "aes-256-gcm"
+	AES128cbc        = "aes-128-cbc"
+	AES192cbc        = "aes-192-cbc"
+	AES256cbc        = "aes-256-cbc"
+	AES128gcm        = "aes-128-gcm"
+	AES192gcm        = "aes-192-gcm"
+	AES256gcm        = "aes-256-gcm"
+	Chacha20Poly1305 = "chacha20-poly1305"
 )
