554a6cdb92d0c05f9e42a1e01d09ec81f86a728d
Quentin McGaw <quentin.mcgaw@gmail.com>
2022-11-11 09:43:07 +0000

    feat(healthcheck): add FAQ url on unhealthy log

--- internal/healthcheck/health.go
+++ internal/healthcheck/health.go
@@ -29,7 +29,7 @@ func (s *Server) runHealthcheckLoop(ctx context.Context, done chan<- struct{}) {
 			s.vpn.healthyTimer.Stop()
 			s.vpn.healthyWait = *s.config.VPN.Initial
 		} else if previousErr == nil && err != nil {
-			s.logger.Info("unhealthy: " + err.Error())
+			s.logger.Info("unhealthy: " + err.Error() + "(see https://github.com/qdm12/gluetun/wiki/Healthcheck)")
 			s.vpn.healthyTimer.Stop()
 			s.vpn.healthyTimer = time.NewTimer(s.vpn.healthyWait)
 		}
