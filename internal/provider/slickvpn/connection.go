8dda482d741d8aa63d9874dc49b8b98624afd16e
Quentin McGaw <quentin.mcgaw@gmail.com>
2023-03-25 12:23:32 +0000

    fix(slickvpn): precise default TCP port as `443`

--- internal/provider/slickvpn/connection.go
+++ internal/provider/slickvpn/connection.go
@@ -8,6 +8,6 @@ import (
 
 func (p *Provider) GetConnection(selection settings.ServerSelection, ipv6Supported bool) (
 	connection models.Connection, err error) {
-	defaults := utils.NewConnectionDefaults(0, 443, 0) //nolint:gomnd
+	defaults := utils.NewConnectionDefaults(443, 443, 0) //nolint:gomnd
 	return utils.GetConnection(p.Name(), p.storage, selection, defaults, ipv6Supported, p.randSource)
 }
