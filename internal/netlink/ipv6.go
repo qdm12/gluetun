b504227625556de99102ce8c139f11768a187970
Quentin McGaw <quentin.mcgaw@gmail.com>
2024-05-09 18:21:13 +0000

    fix(ipv6): detect ignoring loopback route destinations

--- internal/netlink/ipv6.go
+++ internal/netlink/ipv6.go
@@ -14,16 +14,21 @@ func (n *NetLink) IsIPv6Supported() (supported bool, err error) {
 	// as IPv6 routes at container start, see:
 	// https://github.com/qdm12/gluetun/issues/1241#issuecomment-1333405949
 	for _, route := range routes {
+		link, err := n.LinkByIndex(route.LinkIndex)
+		if err != nil {
+			return false, fmt.Errorf("finding link corresponding to route: %w", err)
+		}
+
 		sourceIsIPv6 := route.Src.IsValid() && route.Src.Is6()
 		destinationIsIPv6 := route.Dst.IsValid() && route.Dst.Addr().Is6()
-		if sourceIsIPv6 || destinationIsIPv6 {
-			link, err := n.LinkByIndex(route.LinkIndex)
-			if err != nil {
-				return false, fmt.Errorf("finding IPv6 supported link: %w", err)
-			}
-			n.debugLogger.Debugf("IPv6 is supported by link %s", link.Name)
-			return true, nil
+		switch {
+		case !sourceIsIPv6 && !destinationIsIPv6,
+			destinationIsIPv6 && route.Dst.Addr().IsLoopback():
+			continue
 		}
+
+		n.debugLogger.Debugf("IPv6 is supported by link %s", link.Name)
+		return true, nil
 	}
 
 	n.debugLogger.Debugf("IPv6 is not supported after searching %d routes",
