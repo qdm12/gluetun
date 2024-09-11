41cd9cea98e62750b65cc340b9e7100dbcef91ea
Quentin McGaw <quentin.mcgaw@gmail.com>
2024-09-11 09:34:35 +0200

    feat(server): add apikey auth method (#2437)

---
+++ internal/server/middlewares/auth/apikey.go
@@ -0,0 +1,34 @@
+package auth
+
+import (
+	"crypto/subtle"
+	"net/http"
+)
+
+type apiKeyMethod struct {
+	apiKey string
+}
+
+func newAPIKeyMethod(apiKey string) *apiKeyMethod {
+	return &apiKeyMethod{
+		apiKey: apiKey,
+	}
+}
+
+// equal returns true if another auth checker is equal.
+// This is used to deduplicate checkers for a particular route.
+func (a *apiKeyMethod) equal(other authorizationChecker) bool {
+	otherTokenMethod, ok := other.(*apiKeyMethod)
+	if !ok {
+		return false
+	}
+	return a.apiKey == otherTokenMethod.apiKey
+}
+
+func (a *apiKeyMethod) isAuthorized(request *http.Request) bool {
+	xAPIKey := request.Header.Get("X-API-Key")
+	if xAPIKey == "" {
+		xAPIKey = request.URL.Query().Get("api_key")
+	}
+	return subtle.ConstantTimeCompare([]byte(xAPIKey), []byte(a.apiKey)) == 1
+}
