58eb69a341c7eee14deea211d7e20e6706925469
shwoop <monkey.mambo.al.ferguson@gmail.com>
2025-09-12 19:52:09 +0200

    Update help text language proposed during code review
    Co-authored-by: Quentin McGaw <quentin.mcgaw@gmail.com>

--- internal/cli/help.go
+++ internal/cli/help.go
@@ -8,10 +8,9 @@ func (c *CLI) Help() {
 	//nolint:lll
 	fmt.Printf("Usage: gluetun [COMMAND [OPTIONS]]\n\nLightweight swiss-army-knife-like VPN client to multiple VPN service providers.\n\n")
 	fmt.Println("Commands:")
-	fmt.Printf("  version        \tPrint the version of gluetun\n")
-	fmt.Printf("  update         \tUpdate the VPN providers and servers\n")
-	fmt.Printf("  healthcheck    \tCheck the health of the VPN connection\n")
+	fmt.Printf("  update         \tUpdate the VPN servers data for some or all providers\n")
+	fmt.Printf("  healthcheck    \tCheck the health of the VPN connection of another Gluetun instance\n")
 	fmt.Printf("  openvpnconfig  \tPrint the OpenVPN configuration (for debugging)\n")
-	fmt.Printf("  format-servers \tFormat the servers into a format that can be used by OpenVPN\n")
-	fmt.Printf("  genkey         \tGenerate a new OpenVPN key\n")
+	fmt.Printf("  format-servers \tFormat the servers data into a Markdown table\n")
+	fmt.Printf("  genkey         \tGenerate a new 32 bytes Wireguard key (base58 encoded)\n")
 }
