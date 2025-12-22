fd6e5e4e902604e4bb59ccd68340fc7c8e369991
Quentin McGaw <quentin.mcgaw@gmail.com>
2025-12-22 22:47:17 +0000

    fix(pia): update default openvpn ports and possible ports to choose - Fix #3046

--- internal/provider/privateinternetaccess/connection.go
+++ internal/provider/privateinternetaccess/connection.go
@@ -3,24 +3,13 @@ package privateinternetaccess
 import (
 	"github.com/qdm12/gluetun/internal/configuration/settings"
 	"github.com/qdm12/gluetun/internal/models"
-	"github.com/qdm12/gluetun/internal/provider/privateinternetaccess/presets"
 	"github.com/qdm12/gluetun/internal/provider/utils"
 )
 
 func (p *Provider) GetConnection(selection settings.ServerSelection, ipv6Supported bool) (
 	connection models.Connection, err error,
 ) {
-	// Set port defaults depending on encryption preset.
-	var defaults utils.ConnectionDefaults
-	switch *selection.OpenVPN.PIAEncPreset {
-	case presets.None, presets.Normal:
-		defaults.OpenVPNTCPPort = 502
-		defaults.OpenVPNUDPPort = 1198
-	case presets.Strong:
-		defaults.OpenVPNTCPPort = 501
-		defaults.OpenVPNUDPPort = 1197
-	}
-
+	defaults := utils.NewConnectionDefaults(8443, 8080, 0) //nolint:mnd
 	return utils.GetConnection(p.Name(),
 		p.storage, selection, defaults, ipv6Supported, p.randSource)
 }
