05e3cf2c90ada922277c709845d2d0c282f6ce87
Quentin McGaw <quentin.mcgaw@gmail.com>
2025-12-23 17:04:28 +0000

    hotfix(publicip/api): fix mutex logic

--- internal/publicip/api/resilient_test.go
+++ internal/publicip/api/resilient_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetMostPopularResult(t *testing.T) {
+func Test_GetMostPopularResult(t *testing.T) {
 	t.Parallel()
 
 	testCases := map[string]struct {
